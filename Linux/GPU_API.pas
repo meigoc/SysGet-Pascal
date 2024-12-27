2d72c2bef2f7eedaf640af65200ce2973945a17b
Meigo <73817505+meigoc@users.noreply.github.com>
2024-12-27 20:26:54 +0300

    ✅ LinuxAPI - Видеокарта (фикс)

--- Linux/GPU_API.pas
+++ Linux/GPU_API.pas
@@ -13,45 +13,39 @@ function GetGPUVendor: string;
 implementation
 
 uses
-  Windows, SysUtils;
+  SysUtils, Classes;
 
 { Функция получения имени видеокарты
 (GPU Name Retrieval Function) }
 function GetGPUName: string;
 var
-  GPUName: array[0..127] of Char;
-  Size: DWORD;
+  Output: TStringList;
+  Command: string;
 begin
-  Size := SizeOf(GPUName);
-  if GetSystemRegistryKey(HKEY_LOCAL_MACHINE, 'HARDWARE\DEVICEMAP\VIDEO\0', 'DriverDesc', @GPUName, Size) then
-    Result := GPUName
-  else
-    Result := 'Unknown';
+  Command := 'lspci | grep VGA';
+  Output := TStringList.Create;
+  try
+    Output.Text := Trim(SysUtils.ExecuteProcess('/bin/sh', ['-c', Command], Output));
+    Result := Output.Text;
+  finally
+    Output.Free;
+  end;
 end;
 
 { Функция получения производителя видеокарты
 (GPU Vendor Retrieval Function) }
 function GetGPUVendor: string;
 var
-  GPUVendor: array[0..127] of Char;
-  Size: DWORD;
+  Output: TStringList;
+  Command: string;
 begin
-  Size := SizeOf(GPUVendor);
-  if GetSystemRegistryKey(HKEY_LOCAL_MACHINE, 'HARDWARE\DEVICEMAP\VIDEO\0', 'ProviderName', @GPUVendor, Size) then
-    Result := GPUVendor
-  else
-    Result := 'Unknown';
-end;
-
-function GetSystemRegistryKey(RootKey: HKEY; Key, Value: PChar; var Data; DataSize: DWORD): Boolean;
-var
-  hKey: HKEY;
-begin
-  Result := RegOpenKeyEx(RootKey, Key, 0, KEY_QUERY_VALUE, hKey) = ERROR_SUCCESS;
-  if Result then
-  begin
-    Result := RegQueryValueEx(hKey, Value, nil, nil, @Data, @DataSize) = ERROR_SUCCESS;
-    RegCloseKey(hKey);
+  Command := 'lspci -nn | grep VGA';
+  Output := TStringList.Create;
+  try
+    Output.Text := Trim(SysUtils.ExecuteProcess('/bin/sh', ['-c', Command], Output));
+    Result := Output.Text;
+  finally
+    Output.Free;
   end;
 end;
 
