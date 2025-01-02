405f6a21aa11cb5c87f08a316b04566eb3feb1da
Meigo <73817505+meigoc@users.noreply.github.com>
2025-01-02 11:56:19 +0300

    SoundCard support

--- test.pas
+++ test.pas
@@ -1,13 +1,13 @@
 program Test;
 
 {$IFDEF MSWINDOWS}
-uses Windows.CPU_API, Windows.GPU_API, Windows.Battery_API, Windows.Monitor_API, Windows.Sensor_API;
+uses Windows.CPU_API, Windows.GPU_API, Windows.Battery_API, Windows.Monitor_API, Windows.Sensor_API, Windows.SoundCard_API;
 {$ENDIF}
 {$IFDEF LINUX}
-uses Linux.CPU_API, Linux.GPU_API, Linux.Monitor_API, Linux.Sensor_API;
+uses Linux.CPU_API, Linux.GPU_API, Linux.Monitor_API, Linux.Sensor_API, Linux.SoundCard_API;
 {$ENDIF}
 {$IFDEF DARWIN}
-uses MacOS_Darwin.CPU_API, MacOS_Darwin.GPU_API, MacOS_Darwin.Monitor_API, MacOS_Darwin.Sensor_API;
+uses MacOS_Darwin.CPU_API, MacOS_Darwin.GPU_API, MacOS_Darwin.Monitor_API, MacOS_Darwin.Sensor_API, MacOS_Darwin.SoundCard_API;
 {$ENDIF}
 
 begin
@@ -25,6 +25,9 @@ begin
 
   writeln('Fan Speed: ', GetFanSpeed);
   writeln('Temperature: ', GetTemperature:0:2);
+
+  writeln('Sound Card Name: ', GetSoundCardName);
+  writeln('Sound Card Vendor: ', GetSoundCardVendor);
 
   {$IFDEF MSWINDOWS}
   if IsBatteryPresent then
